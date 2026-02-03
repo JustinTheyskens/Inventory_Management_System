96e768ad1b06b3be361ecea25524e2f4a02814f4
JustinTheyskens <justin.theyskens@stonybrook.edu>
2026-02-03 09:26:49 -0500

    started add/remove/transfer logic

--- Inventory_Management_System/Back_End/src/Repos/Inventory.Repo.js
+++ Inventory_Management_System/Back_End/src/Repos/Inventory.Repo.js
@@ -2,14 +2,14 @@ import { Inventory } from "../Models/Inventory.Model.js";
 
 const InventoryRepo = {
 
-    findByWarehouse: (warehouseId) => Inventory.find(
-        {
-            warehouse : warehouseId
-        }
-    ).populate("item"),
+    findByWarehouse: (warehouseId) => Inventory.find({warehouse : warehouseId}).populate("item"),
+
     findByItem: (itemId) => Inventory.find(itemId).populate("warehouse"),
+
     findOne: (itemId, warehouseId) => Inventory.findOne({itemId, warehouseId}),
+
     create: (data) => Inventory.create(data),
+
     update: (itemId, warehouseId, quantity) => 
         Inventory.findOneAndUpdate(
         {
@@ -22,7 +22,8 @@ const InventoryRepo = {
         {
             new: true
         }
-    )  
+    )
+
 };
 
 export default InventoryRepo;
--- Inventory_Management_System/Back_End/src/Services/Inventory.Service.js
+++ Inventory_Management_System/Back_End/src/Services/Inventory.Service.js
@@ -3,6 +3,61 @@ import InventoryRepo from "../Repos/Inventory.Repo.js";
 const InventoryService = {
     getByWarehouse: async (warehouseId) => {
         return await InventoryRepo.findByWarehouse(warehouseId);
+    },
+
+    add: async (itemId, warehouseId, amount) => {
+        if (amount < 0)
+        {
+            throw new Error("Amount cannot be below 0.");  
+        }
+
+        const existingInventory = await InventoryRepo.findOne(itemId, warehouseId);
+
+        if (existingInventory)
+        {
+            return await InventoryRepo.update(itemId, warehouseId, existingInventory.quantity + amount);
+        }
+
+        return await InventoryRepo.create(
+            {
+                item: itemId,
+                warehouse: warehouseId,
+                quantity: amount
+            });
+    },
+
+    remove: async (itemId, warehouseId, amount) => {
+        if (amount <= 0) {
+            throw new Error("Amount must be greater than 0.");
+        }
+
+        const existingInventory = await InventoryRepo.findOne(itemId, warehouseId);
+
+        if (!existingInventory)
+        {
+            throw new Error("Inventory record not found.");
+        }
+
+        if (existingInventory.quantity < amount)
+        {
+            throw new Error("Insufficiant stock.");
+        }
+
+        return await InventoryRepo.update(itemId, warehouseId, existingInventory.quantity - amount);
+    },
+
+    transfer: async(itemId, fromWarehouseId, toWarehouseId, amount) => {
+        if (fromWarehouseId == toWarehouseId)
+        {
+            throw new Error("scource & destination warehouses must be different.");
+        }
+
+        // remove from source
+        await InventoryService.remove(itemId, fromWarehouseId, amount);
+
+        // then add to dest.
+        return await InventoryService.add(itemId, toWarehouseId, amount);
+
     }
 };
 
