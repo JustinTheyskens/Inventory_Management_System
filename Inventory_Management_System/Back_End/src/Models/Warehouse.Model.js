6a1f53149efd846e034307678ebe7451f835de62
JustinTheyskens <justin.theyskens@stonybrook.edu>
2026-02-03 15:00:37 -0500

    finished write logic / updated warehouse schema

--- Inventory_Management_System/Back_End/src/Models/Warehouse.Model.js
+++ Inventory_Management_System/Back_End/src/Models/Warehouse.Model.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 
 const warehouseSchema = new mongoose.Schema({
 
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
     location : String,
     max_capacity : Number,
     current_capacity : Number,
